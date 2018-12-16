c41eb53c7cf714c4d0fd7ec9a1002151d752442b
alu0100956955 <43880851+alu0100956955@users.noreply.github.com>
2018-12-16 22:28:30 +0000

    Añado el trigger , sin testear

---
+++ trigger_vivero.sql
@@ -0,0 +1,30 @@
+
+-- Primero voy a cambiar el tipo de dato de bonificacion 
+-- de varchar a int
+
+ALTER TABLE mydb.cliente ALTER COLUMN bonificacion TYPE INT;
+
+
+-- Trigger
+CREATE FUNCTION boni() RETURNS trigger AS $boni$
+	BEGIN
+		IF NEW.total_ventas > 10 THEN
+			UPDATE mydb.Cliente SET NEW.bonificacion = 10
+		END IF;
+
+		IF NEW.total_ventas > 15 THEN
+			UPDATE mydb.Cliente SET NEW.bonificacion = 15	
+		END IF;
+
+		IF NEW.total_ventas > 20 THEN
+			UPDATE mydb.Cliente SET NEW.bonificacion = 20
+		END IF;
+
+		IF NEW.total_ventas > 30 THEN
+			UPDATE mydb.Cliente SET NEW.bonificacion = 25
+		END IF;
+	END;
+$boni$ LANGUAGE plpgsql;
+
+CREATE TRIGGER boni BEFORE INSERT OR UPDATE ON mydb.Cliente
+	FOR EACH ROW EXECUTE PROCEDURE boni();
